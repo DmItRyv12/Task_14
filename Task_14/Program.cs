7a90f553e00261a03dfffaa997ab67ed46c7de89
Дмитрий Минаков <m1nak0vd@yandex.ru>
2022-01-16 20:55:44 +0300

    Добавьте файлы проекта.

---
+++ Task_14/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string Name = "Кошка";
+            Console.WriteLine("Введите Кошка или Собака");
+            Name = Console.ReadLine();
+            Animal say = new Animal(Name);
+            Console.WriteLine(say.Say());
+            Main(args);
+        }
+
+        abstract public class AbstractAnimal
+        {
+            public abstract string Name { set; get; }
+            public string say { set; get; }
+            public abstract string Say();
+        }
+
+        public class Animal : AbstractAnimal
+        {
+            public override string Name { set; get; }
+            public Animal(string name)
+            {
+                this.Name = name;
+            }
+            public override string Say()
+            {
+                if (Name == "Кошка")
+                {
+                    say = "Мяу";
+                }
+                else
+                {
+                    say = "Гав";
+                }
+                return string.Format("{0}", say);
+            }
+        }
+    }
+}
